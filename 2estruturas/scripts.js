1fbe312b18d1bc0e4836a2d02401bbe5143c5b72
samueljdev <samuel.jdev11@gmail.com>
2025-05-12 01:06:55 -0400

    Variáveis (salvando valores) ==

---
+++ 2estruturas/scripts.js
@@ -0,0 +1,15 @@
+// 1 - Variáveis (salvando valores)
+console.log("Variáveis (salvando valores) == ")
+
+let nome = "Samuel";
+console.log(nome);
+
+nome = "Samuel Lisboa";
+console.log(nome);
+
+const idade = 31;
+console.log(idade);
+// idade = 33;
+
+console.log(typeof nome)
+console.log(typeof idade)
