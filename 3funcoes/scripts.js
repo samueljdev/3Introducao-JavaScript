941fd1790150125e9e5234f27c8686a204ea532d
samueljdev <samuel.jdev11@gmail.com>
2025-05-12 01:24:36 -0400

    Escopo aninhado ==

--- 3funcoes/scripts.js
+++ 3funcoes/scripts.js
@@ -49,3 +49,23 @@ function testandoEscopo() {
 console.log(`Y fora da função: ${y}`);
 
 testandoEscopo();
+
+// 4 - escopo aninhado
+console.log("Escopo aninhado ==");
+
+let m = 10;
+
+function escopoAninhado() {
+  let m = 20;
+
+  if (true) {
+    let m = 30;
+
+    console.log(m);
+  }
+  console.log(m);
+}
+
+escopoAninhado();
+
+console.log(m);
