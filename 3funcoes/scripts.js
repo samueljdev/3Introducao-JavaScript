137272b17fe3d0451ede5744898b5a17894294fb
samueljdev <samuel.jdev11@gmail.com>
2025-05-12 01:22:40 -0400

    console.log("Return ==");

--- 3funcoes/scripts.js
+++ 3funcoes/scripts.js
@@ -18,3 +18,20 @@ function funcaoComParametro(txt) {
 }
 
 funcaoComParametro("Opa");
+
+// 2 - return
+console.log("Return ==");
+
+const a = 10;
+const b = 20;
+const c = 30;
+const d = 40;
+
+function soma(n1, n2) {
+  return n1 + n2;
+}
+
+const resultado = soma(a, b);
+console.log(resultado);
+
+console.log(soma(c, d));
