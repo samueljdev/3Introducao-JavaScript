36653c6724ffaeb31984f241a5a2283a8c80897c
samueljdev <samuel.jdev11@gmail.com>
2025-05-13 21:57:38 -0400

    Conhecendo melhor os objetos ==

--- 4arrays-objetos/scripts.js
+++ 4arrays-objetos/scripts.js
@@ -93,3 +93,12 @@ console.log(obj2);
 
 Object.assign(obj2, obj);
 console.log(obj2);
+
+// 8 - conhecendo melhor os objetos
+console.log("Conhecendo melhor os objetos ==");
+
+console.log(Object.keys(obj));
+console.log(Object.keys(obj2));
+console.log(Object.keys(person));
+
+console.log(Object.entries(obj));
