ad853f2bbe888531636beb0beea44b735c6f6414
samueljdev <samuel.jdev11@gmail.com>
2025-05-15 22:38:10 -0400

    Rest operator ==

--- 4arrays-objetos/scripts.js
+++ 4arrays-objetos/scripts.js
@@ -288,3 +288,19 @@ console.log("Repeat ==");
 
 const palavra = "Testando, ";
 console.log(palavra.repeat(5));
+
+// 23 - rest operator
+console.log("Rest operator ==");
+
+const somaInfinita = (...args) => {
+  let total = 0;
+
+  for (let i = 0; i < args.length; i++) {
+    total += args[i];
+  }
+  return total;
+}
+
+console.log(somaInfinita(1, 5, 10));
+
+console.log(somaInfinita(1, 2, 3, 4, 5, 6, 7, 7, 8, 9));
