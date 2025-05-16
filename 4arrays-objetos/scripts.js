0af713941a3b2f5067f399a6f4f0a3105eec0c53
samueljdev <samuel.jdev11@gmail.com>
2025-05-15 22:41:17 -0400

    For...of ==

--- 4arrays-objetos/scripts.js
+++ 4arrays-objetos/scripts.js
@@ -304,3 +304,18 @@ const somaInfinita = (...args) => {
 console.log(somaInfinita(1, 5, 10));
 
 console.log(somaInfinita(1, 2, 3, 4, 5, 6, 7, 7, 8, 9));
+
+// 24 - for...of
+console.log("For...of ==");
+
+const somaInfinita2 = (...args) => {
+  let total, num = 0;
+
+  for (num of args) {
+    total += num;
+  }
+  return total;
+}
+
+console.log(somaInfinita2(1, 5, 10));
+console.log(somaInfinita2(1, 2, 3, 4, 5, 6, 7, 7, 8, 9));
