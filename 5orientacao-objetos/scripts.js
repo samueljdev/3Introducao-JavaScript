89cf96ebae02a3aefcfbf8c61949e2ec6e06ef72
samueljdev <samuel.jdev11@gmail.com>
2025-05-17 00:20:54 -0400

    Override por prototype ==

--- 5orientacao-objetos/scripts.js
+++ 5orientacao-objetos/scripts.js
@@ -165,3 +165,20 @@ Caminhao.prototype.motor = 4.0;
 const c3 = new Caminhao(6, "Azul");
 console.log(c3);
 console.log(c3.motor);
+
+// 11 - override por prototype
+console.log("Override por prototype ==");
+
+class Humano {
+  constructor(nome, idade) {
+    this.nome = nome;
+    this.idade = idade;
+  }
+}
+
+const matheus = new Humano("Matheus", 31);
+console.log(matheus);
+
+Humano.prototype.idade = "Não definida";
+console.log(matheus.idade);
+console.log(Humano.prototype.idade);
