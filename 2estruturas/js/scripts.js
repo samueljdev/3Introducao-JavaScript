b871698e325412a54af11be47f289b3d9af0c024
samueljdev <samuel.jdev11@gmail.com>
2025-06-05 12:02:56 -0400

    Math

--- 2estruturas/js/scripts.js
+++ 2estruturas/js/scripts.js
@@ -50,3 +50,11 @@ console.log(_teste, $teste);
 // const z = 10;
 
 // alert(`O número é ${z}`);
+
+// 5 - Math
+
+console.log(Math.max(5, 2, 1, 10));
+
+console.log(Math.floor(5.14));
+
+console.log(Math.ceil(5.14));
