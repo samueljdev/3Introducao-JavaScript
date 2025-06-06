59743ee19d9355c2a4a025d506dcb2d94f833f8e
samueljdev <samuel.jdev11@gmail.com>
2025-06-06 12:41:18 -0400

    Mais sobre closure

--- 3funcoes/js/scripts.js
+++ 3funcoes/js/scripts.js
@@ -151,3 +151,20 @@ function someFunction() {
 }
 
 someFunction();
+
+// 10 - mais sobre closure
+const multiplicationClosure = (n) => {
+    return (m) => {
+        return n * m;
+    };
+};
+
+const c1 = multiplicationClosure(5);
+
+const c2 = multiplicationClosure(10);
+
+console.log(c1);
+console.log(c2);
+
+console.log(c1(5));
+console.log(c2(10));
