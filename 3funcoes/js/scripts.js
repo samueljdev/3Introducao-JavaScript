6860a64e609aedb0ccd14e90eba68dd38ea7b482
samueljdev <samuel.jdev11@gmail.com>
2025-06-06 09:52:45 -0400

    Valor default

--- 3funcoes/js/scripts.js
+++ 3funcoes/js/scripts.js
@@ -120,3 +120,21 @@ const greeting = (name) => {
 
 greeting();
 greeting("Matheus");
+
+// 8 - valor default
+const customGreeting = (name, greet = "Olá") => {
+    return `${greet}, ${name}!`;
+};
+
+console.log(customGreeting("Matheus"));
+
+console.log(customGreeting("João", "Bom dia"));
+
+const repeatText = (text, repeat = 2) => {
+    for (let i = 0; i < repeat; i++) {
+        console.log(text);
+    }
+};
+
+repeatText("Testando");
+repeatText("Agora repete 5 vezes", 5);
