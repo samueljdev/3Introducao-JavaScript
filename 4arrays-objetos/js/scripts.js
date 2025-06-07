5334d796fe55dc3af97d955960dacb35cab8ae47
samueljdev <samuel.jdev11@gmail.com>
2025-06-07 11:06:13 -0400

    Métodos

--- 4arrays-objetos/js/scripts.js
+++ 4arrays-objetos/js/scripts.js
@@ -27,3 +27,18 @@ console.log(numbers["length"]);
 const myName = "Matheus";
 
 console.log(myName.length);
+
+// 4 - métodos
+const otherNumbers = [1, 2, 3];
+
+const allNumbers = numbers.concat(otherNumbers);
+
+console.log(allNumbers);
+
+const text = "algum texto";
+
+console.log(text.toUpperCase());
+
+console.log(typeof text.toUpperCase);
+
+console.log(text.indexOf("g"));
