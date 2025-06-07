b1ae8f180481cca217c1898fbadb4682016c46f7
samueljdev <samuel.jdev11@gmail.com>
2025-06-07 11:04:08 -0400

    Mais sobre arrays

--- 4arrays-objetos/js/scripts.js
+++ 4arrays-objetos/js/scripts.js
@@ -8,3 +8,12 @@ console.log(typeof lista);
 const itens = ["Matheus", true, 2, 4.12];
 
 console.log(itens);
+
+// 2 - mais sobre arrays
+const arr = ["a", "b", "c", "d"];
+
+console.log(arr[0]);
+
+console.log(arr[2]);
+
+console.log(arr[10]);
