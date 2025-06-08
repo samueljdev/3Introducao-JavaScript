378d50d0a055c67246e90951bd80fbda2ca9d9dd
samueljdev <samuel.jdev11@gmail.com>
2025-06-08 00:42:25 -0400

    Symbol

--- 5orientacao-objetos/js/scripts.js
+++ 5orientacao-objetos/js/scripts.js
@@ -168,3 +168,23 @@ Humano.prototype.idade = "Não definida";
 
 console.log(matheus.idade);
 console.log(Humano.prototype.idade);
+
+// 12 - symbol
+class Aviao {
+    constructor(marca, turbinas) {
+        this.marca = marca;
+        this.turbinas = turbinas;
+    }
+}
+
+const asas = Symbol();
+
+Aviao.prototype[asas] = 2;
+
+const boeing = new Aviao("Boeing", 10);
+
+console.log(boeing);
+
+console.log(boeing[asas]);
+
+console.log(Aviao.prototype[asas]);
