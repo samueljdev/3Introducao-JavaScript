4a38cba314755856975879ed846bc6cdf1415a32
samueljdev <samuel.jdev11@gmail.com>
2025-06-08 00:11:10 -0400

    Mais sobre classes

--- 5orientacao-objetos/js/scripts.js
+++ 5orientacao-objetos/js/scripts.js
@@ -120,3 +120,34 @@ const jeff = new CachorroClasse("Jeff", "Labrador");
 console.log(jeff);
 
 console.log(Object.getPrototypeOf(jeff));
+
+// 10 - mais sobre classes
+class Caminhao {
+    constructor(eixos, cor) {
+        this.eixos = eixos;
+        this.cor = cor;
+    }
+
+    descreverCaminhao() {
+        console.log(
+            `Este caminhão tem ${this.eixos} eixos e é da cor ${this.cor}.`
+        );
+    }
+}
+
+const scania = new Caminhao(6, "Vermelha");
+
+scania.descreverCaminhao();
+
+Caminhao.motor = 4.0;
+
+const c2 = new Caminhao(4, "Preta");
+
+// Motor é undefined
+console.log(c2.motor);
+
+Caminhao.prototype.motor = 4.0;
+
+const c3 = new Caminhao(6, "Azul");
+
+console.log(c3.motor);
