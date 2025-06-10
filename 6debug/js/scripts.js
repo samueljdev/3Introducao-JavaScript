56d5c80c2ae74e1d3c7eb6319024d3e326957666
samueljdev <samuel.jdev11@gmail.com>
2025-06-09 20:09:20 -0400

    Debugger

--- 6debug/js/scripts.js
+++ 6debug/js/scripts.js
@@ -29,3 +29,25 @@ if (a > 5) {
 }
 
 console.log(a);
+
+// 3 - debugger
+let c = 1;
+let d = 2;
+
+if (c == 1) {
+    c = d + 2;
+}
+
+// debugger;
+
+for (let i = 0; i < d; i++) {
+    c = c + 2;
+}
+
+// debugger;
+
+if (c > 5) {
+    c = 25;
+}
+
+// debugger;
