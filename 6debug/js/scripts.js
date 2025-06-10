7a9e35b9295aab57694c28f3f38182396b6b4724
samueljdev <samuel.jdev11@gmail.com>
2025-06-09 20:10:05 -0400

    Tratamento de dados com function

--- 6debug/js/scripts.js
+++ 6debug/js/scripts.js
@@ -51,3 +51,21 @@ if (c > 5) {
 }
 
 // debugger;
+
+// 4 - tratamento de dados com function
+function checkNumber(n) {
+    const result = Number(n);
+
+    if (Number.isNaN(result)) {
+        console.log("Valor incorreto!");
+        return null;
+    } else {
+        console.log("Valor correto!");
+        return result;
+    }
+}
+
+checkNumber(5);
+checkNumber("10");
+checkNumber({});
+checkNumber("teste");
