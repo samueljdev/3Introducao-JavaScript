5c27856834033c9c16d08295b8609f27395e4e56
samueljdev <samuel.jdev11@gmail.com>
2025-06-10 11:29:10 -0400

    Generators

--- 7async/js/scripts.js
+++ 7async/js/scripts.js
@@ -104,3 +104,16 @@ async function chamadaAsync() {
 }
 
 chamadaAsync();
+
+// 9 - Generators
+function* generator() {
+    yield 1;
+    yield 2;
+    yield 3;
+}
+
+const gen = generator();
+
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next().value);
