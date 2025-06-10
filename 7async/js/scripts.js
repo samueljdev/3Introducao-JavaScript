fe2522ff47252a33d4cb1f8d279604d059db90bd
samueljdev <samuel.jdev11@gmail.com>
2025-06-10 11:22:07 -0400

    setInterval

--- 7async/js/scripts.js
+++ 7async/js/scripts.js
@@ -6,3 +6,12 @@ setTimeout(function () {
 }, 2000);
 
 console.log("Ainda não executou 2");
+
+// 2 - setInterval
+console.log("Ainda não começou");
+
+// setInterval(function () {
+//     console.log("Intervalo assíncrono");
+// }, 3000);
+
+console.log("Ainda não começou 2");
