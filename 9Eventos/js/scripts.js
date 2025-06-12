d41f66376e5ccf9e55e26073e93a211a6fcce7af
samueljdev <samuel.jdev11@gmail.com>
2025-06-12 12:18:32 -0400

    Debounce

--- 9Eventos/js/scripts.js
+++ 9Eventos/js/scripts.js
@@ -110,3 +110,23 @@ window.addEventListener("load", () => {
 window.addEventListener("beforeunload", (event) => {
     event.preventDefault();
 });
+
+// 12 - debounce
+const debounce = (f, delay) => {
+
+    let timeout;
+    return (...arguments) => {
+        if (timeout) {
+            clearTimeout(timeout);
+        }
+
+        timeout = setTimeout(() => {
+            f.apply(arguments);
+        }, delay);
+    };
+};
+
+window.addEventListener("mousemove", debounce(() => {
+    console.log("Executando a cada 400ms");
+}, 400)
+);
