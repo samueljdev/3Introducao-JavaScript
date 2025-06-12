45065707d0b13351b7dcc9fe98a041f75d75cb6a
samueljdev <samuel.jdev11@gmail.com>
2025-06-12 12:08:34 -0400

    Propagação de evento

--- 9Eventos/js/scripts.js
+++ 9Eventos/js/scripts.js
@@ -31,3 +31,17 @@ title.addEventListener("click", (e) => {
     console.log(e.offsetX); // posição do elemento
     console.log(e.pointerType);
 });
+
+// 4 - propagação
+const containerBtn = document.querySelector("#btn-container");
+const btnInsideContainer = document.querySelector("#div-btn");
+
+containerBtn.addEventListener("click", () => {
+    console.log("Evento 1");
+});
+
+btnInsideContainer.addEventListener("click", (e) => {
+    // sem isso acontece a propagação
+    e.stopPropagation();
+    console.log("Evento 2");
+});
