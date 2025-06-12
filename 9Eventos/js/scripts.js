61ba0cd5505fc726677fa59fbe8f166f5bac8700
samueljdev <samuel.jdev11@gmail.com>
2025-06-12 11:59:39 -0400

    Removendo evento

--- 9Eventos/js/scripts.js
+++ 9Eventos/js/scripts.js
@@ -4,3 +4,19 @@ const btn = document.querySelector("#my-button");
 btn.addEventListener("click", function () {
     console.log("Clicou aqui!");
 });
+
+// 2 - removendo evento
+const secondBtn = document.querySelector("#btn");
+
+function imprimirMensagem() {
+    console.log("Teste");
+}
+
+secondBtn.addEventListener("click", imprimirMensagem);
+
+const thirdBtn = document.querySelector("#other-btn");
+
+thirdBtn.addEventListener("click", () => {
+    console.log("Evento removido");
+    secondBtn.removeEventListener("click", imprimirMensagem);
+});
