f21e0a194a3e03bf79037708c279325c88bd8374
samueljdev <samuel.jdev11@gmail.com>
2025-06-14 11:29:17 -0400

    Heranca

--- 10JS-Moderno/js/scripts.js
+++ 10JS-Moderno/js/scripts.js
@@ -169,3 +169,23 @@ const shirt = new Product("Camisa gola V", 20);
 
 console.log(shirt.productWithDiscount(10));
 console.log(shirt.productWithDiscount(20));
+
+// 9 heranca
+class ProductWithAttributes extends Product {
+    constructor(name, price, colors) {
+        super(name, price);
+        this.colors = colors;
+    }
+
+    showColors() {
+        console.log("As cores são: ");
+        this.colors.forEach((color) => {
+            console.log(color);
+        });
+    }
+}
+
+const hat = new ProductWithAttributes("Chapéu", 29.99, ["Preto", "Amarelo", "Verde",]);
+
+console.log(hat.name);
+hat.showColors()
