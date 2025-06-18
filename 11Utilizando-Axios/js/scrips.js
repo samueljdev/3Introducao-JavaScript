609ecef591ec6a09e87064f33221480bf3741333
samueljdev <samuel.jdev11@gmail.com>
2025-06-18 05:28:29 -0400

    Imprimir dados na tela

--- 11Utilizando-Axios/js/scrips.js
+++ 11Utilizando-Axios/js/scrips.js
@@ -23,3 +23,28 @@ const getData = async () => { // função async espera os dados chegarem
 };
 
 getData();
+
+// 3 - imprimir dados na tela
+const container = document.querySelector("#user-container");
+
+const printData = async () => {
+    const data = await getData();
+
+    console.log(data);
+
+    data.forEach((user) => {
+        const div = document.createElement("div");
+        const nameElement = document.createElement("h2");
+
+        nameElement.textContent = user.name;
+        div.appendChild(nameElement);
+
+        const emailElement = document.createElement("p");
+        emailElement.textContent = user.email;
+        div.appendChild(emailElement);
+
+        container.appendChild(div);
+    });
+};
+
+printData();
