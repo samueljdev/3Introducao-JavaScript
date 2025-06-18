a6441ed33a4e90935e020ca8f57cbaaf7218de61
samueljdev <samuel.jdev11@gmail.com>
2025-06-18 05:12:02 -0400

    Primeiro request

--- 11Utilizando-Axios/js/scrips.js
+++ 11Utilizando-Axios/js/scrips.js
@@ -1,2 +1,25 @@
 // 1 - instalando o axios
 console.log(axios);
+
+// 2 - primeiro request
+const getData = async () => { // função async espera os dados chegarem
+    try {
+        const response = await axios.get("https://jsonplaceholder.typicode.com/users",
+            //   Ver request headers
+            {
+                headers: {
+                    "content-type": "application/json",
+                    custom: "header",
+                },
+            }
+        );
+
+        console.log(response);
+        return response.data;
+
+    } catch (error) {
+        console.log(error.response);
+    }
+};
+
+getData();
