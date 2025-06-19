801db38f3607856677d0db2c42952765917e212d
samueljdev <samuel.jdev11@gmail.com>
2025-06-18 22:56:10 -0400

    Método Post

--- 11Utilizando-Axios/js/scrips.js
+++ 11Utilizando-Axios/js/scrips.js
@@ -49,3 +49,17 @@ const printData = async () => {
 };
 
 printData();
+
+// 5 - post
+const form = document.querySelector("#post-form");
+const titleInput = document.querySelector("#title");
+const bodyInput = document.querySelector("#body");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    axios.post("https://jsonplaceholder.typicode.com/posts",
+        {
+            title: titleInput.value, body: bodyInput.value, userId: 1,
+        });
+});
